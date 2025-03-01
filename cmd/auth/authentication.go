0726b038ee7a305bebbc43b7cd6d4cfa3f683a08
Kartik Shukla <kaarikshukla7120@gmail.com>
2025-03-01 15:13:32 +0000

    Added some changes to handling logic of Register function and added Login funcition

--- cmd/auth/authentication.go
+++ cmd/auth/authentication.go
@@ -1,9 +1,9 @@
+// filepath: /home/kartik7120/booking_auth_service/cmd/auth/authentication.go
 package auth
 
 import (
-	"crypto/ecdsa"
+	"encoding/base64"
 	"errors"
-	"fmt"
 	"os"
 	"time"
 
@@ -11,6 +11,7 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/joho/godotenv"
 	"github.com/kartik7120/booking_auth_service/cmd/helper"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Authentication struct {
@@ -24,10 +25,15 @@ type User struct {
 	Password string `json:"password" validate:"required,alphanum"`
 }
 
+type LoginUser struct {
+	Username string `json:"username" validate:"required,alphanum"`
+	Password string `json:"password" validate:"required,alphanum"`
+}
+
 var validate *validator.Validate
 
 var (
-	key *ecdsa.PrivateKey
+	key []byte
 	t   *jwt.Token
 	s   string
 )
@@ -60,24 +66,36 @@ func (a *Authentication) Register(user User) (string, error) {
 
 	// Check if a user with the same username is present in the database
 
-	queryString := fmt.Sprintf("SELECT * FROM users WHERE username = %s", user.Username)
+	queryString := "SELECT * FROM users WHERE username = ?"
 
-	result, err := a.DB.SelectDB(queryString)
+	result, err := a.DB.SelectDB(queryString, user.Username)
 
 	if err != nil {
 		return "", err
 	}
 
-	if result != nil {
+	cols, err := result.Columns()
+
+	if err != nil {
+		return "", err
+	}
+
+	if len(cols) > 0 {
 		errString := "user with the same username already exists"
 		return "", errors.New(errString)
 	}
 
 	// if user is present then sent an error message that user cannot be created as a user with the same username already exists
 
-	key := os.Getenv("JWT_KEY")
+	keyBase64 := os.Getenv("JWT_KEY_BASE64")
+	keyBytes, err := base64.StdEncoding.DecodeString(keyBase64)
+	if err != nil {
+		return "", err
+	}
 
-	t = jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
+	key = keyBytes
+
+	t = jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"sub":  user.Username,
 		"exp":  time.Now().Add(time.Hour * 72).Unix(),
 		"role": "user",
@@ -90,11 +108,104 @@ func (a *Authentication) Register(user User) (string, error) {
 		return "", err
 	}
 
-	// if a user with the same username does not exists then we create a jwt token with the sub set to username of the user
+	// hash the password field of the user using bcrypt
+
+	hashedPassword, err := helper.HashPassword(user.Password)
+
+	if err != nil {
+		return "", err
+	}
+
+	// Add user to the database
+
+	queryString = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)"
+
+	_, err = a.DB.InsertDB(queryString, user.Username, user.Email, hashedPassword)
+
+	if err != nil {
+		return "", err
+	}
 
 	return s, err
 }
 
-func (a *Authentication) Login(user User) (string, error) {
-	return "", nil
+func (a *Authentication) Login(user LoginUser) (string, error) {
+
+	err := godotenv.Load()
+
+	if err != nil {
+		return "", err
+	}
+
+	err = validate.Struct(user)
+
+	if err != nil {
+		return "", err
+	}
+
+	if user.Username == "" || user.Password == "" {
+		errString := "username and password are required fields"
+		return "", errors.New(errString)
+	}
+
+	// Check if a user with the same username is present in the database
+
+	queryString := "SELECT * FROM users WHERE username = ?"
+
+	result, err := a.DB.SelectDB(queryString, user.Username)
+
+	if err != nil {
+		return "", err
+	}
+
+	cols, err := result.Columns()
+
+	if err != nil {
+		return "", err
+	}
+
+	if len(cols) == 0 {
+		errString := "user does not exist"
+		return "", errors.New(errString)
+	}
+
+	// extract password field from the result and compare it with the password field of the user
+
+	// if the password does not match then return an error message
+
+	password := "" // extract password from the result
+
+	err = bcrypt.CompareHashAndPassword([]byte(password), []byte(user.Password))
+
+	if err != nil {
+		errString := "password does not match"
+		return "", errors.New(errString)
+	}
+
+	// if the password matches then generate a jwt token and return it
+
+	keyBase64 := os.Getenv("JWT_KEY_BASE64")
+
+	keyBytes, err := base64.StdEncoding.DecodeString(keyBase64)
+
+	if err != nil {
+		return "", err
+	}
+
+	key = keyBytes
+
+	t = jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub":  user.Username,
+		"exp":  time.Now().Add(time.Hour * 72).Unix(),
+		"role": "user",
+		"iss":  "auth-service",
+	})
+
+	s, err = t.SignedString(key)
+
+	if err != nil {
+		return "", err
+	}
+
+	return s, nil
 }
