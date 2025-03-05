f80f94f69c58e4e443e41a35d4485b04331c5e1e
Kartik Shukla <kaarikshukla7120@gmail.com>
2025-03-05 15:34:48 +0000

    Added different models for User table in gorm

---
+++ cmd/models/user.go
@@ -0,0 +1,16 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Username string `json:"username" validate:"required,alphanum" gorm:"unique"`
+	Email    string `json:"email" validate:"required,email" gorm:"unique"`
+	Password string `json:"password" validate:"required,alphanum"`
+	Role     string `json:"role" gorm:"default:USER"`
+}
+
+type LoginUser struct {
+	Username string `json:"username" validate:"required,alphanum"`
+	Password string `json:"password" validate:"required,alphanum"`
+}
