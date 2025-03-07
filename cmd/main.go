d3efb11a0f381c108164d0c0414d8f777274fb03
Kartik Shukla <kaarikshukla7120@gmail.com>
2025-03-07 02:38:27 +0000

    Added stub code to start a microservice

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,48 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"net"
 	"os"
+	"os/signal"
+	"syscall"
 
-	grpcserver "github.com/kartik7120/booking_auth_service/cmd/api/grpcServer"
-	"github.com/kartik7120/booking_auth_service/cmd/helper"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	auth "github.com/kartik7120/booking_auth_service/cmd/grpcServer"
+	"github.com/kartik7120/booking_auth_service/cmd/grpcServer/server"
 )
 
 func main() {
-	conn, err := helper.ConnectToDB()
-	defer conn.Close()
+	lis, err := net.Listen("tcp", ":1101")
 
 	if err != nil {
-		log.Println("Could not connect to postgres database")
-		os.Exit(1)
+		panic(err)
 	}
 
-	api := grpcserver.Config{
-		DbConfig: helper.DBConfig{
-			Conn: conn,
-		},
-	}
+	// Creating a new grpc server
 
+	signalChan := make(chan os.Signal, 1)
+
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+
+	auth.RegisterAuthServiceServer(grpcServer, &server.AuthService{})
+
+	reflection.Register(grpcServer)
+
+	go func() {
+		fmt.Println("Auth Service started")
+		if err := grpcServer.Serve(lis); err != nil {
+			panic(err)
+		}
+	}()
+
+	<-signalChan
+
+	fmt.Println("Shutting down the auth service")
+	grpcServer.GracefulStop()
 }
