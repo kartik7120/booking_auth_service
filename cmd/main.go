7d2a5b14995a26a5d00edc3f24345192bd0758e7
Kartik Shukla <kaarikshukla7120@gmail.com>
2025-03-10 03:58:34 +0000

    Added logic to connect to the database

--- cmd/main.go
+++ cmd/main.go
@@ -7,26 +7,37 @@ import (
 	"os/signal"
 	"syscall"
 
+	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
 	"github.com/joho/godotenv"
+	a "github.com/kartik7120/booking_auth_service/cmd/auth"
 	auth "github.com/kartik7120/booking_auth_service/cmd/grpcServer"
 	"github.com/kartik7120/booking_auth_service/cmd/grpcServer/server"
+	"github.com/kartik7120/booking_auth_service/cmd/helper"
 )
 
 func main() {
 	err := godotenv.Load()
+	log.SetOutput(os.Stdout)
+	log.SetReportCaller(true)
+
+	if os.Getenv("ENV") == "production" {
+		log.SetLevel(log.InfoLevel)
+	} else {
+		log.SetLevel(log.DebugLevel)
+	}
 
 	if err != nil {
-		fmt.Println("Error loading .env file")
+		log.Error("Error loading .env file")
 		panic(err)
 	}
 
 	lis, err := net.Listen("tcp", ":1101")
 
 	if err != nil {
-		fmt.Println("Error starting the server")
+		log.Error("Error starting the server")
 		panic(err)
 	}
 
@@ -39,12 +50,28 @@ func main() {
 	var opts []grpc.ServerOption
 	grpcServer := grpc.NewServer(opts...)
 
-	auth.RegisterAuthServiceServer(grpcServer, &server.AuthService{})
+	// Connect to database
+
+	DB, err := helper.ConnectToDB()
+
+	if err != nil {
+		log.Error("Error connecting to database")
+		panic(err)
+	}
+
+	authObj := a.NewAuthentication(10)
+	authObj.DB = &helper.DBConfig{
+		Conn: DB,
+	}
+
+	auth.RegisterAuthServiceServer(grpcServer, &server.AuthService{
+		Authentication: authObj,
+	})
 
 	reflection.Register(grpcServer)
 
 	go func() {
-		fmt.Println("Auth Service started")
+		log.Info("Starting the auth service")
 		if err := grpcServer.Serve(lis); err != nil {
 			fmt.Println("Error starting the server")
 			panic(err)
@@ -53,6 +80,6 @@ func main() {
 
 	<-signalChan
 
-	fmt.Println("Shutting down the auth service")
+	log.Info("Stopping the server")
 	grpcServer.GracefulStop()
 }
